c7211f5be239ba158aa4804e38bf4ecc21ba65f7
Stanislav Nemchenko <stas.buka@gmail.com>
2025-09-02 18:34:45 +0300

    add SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./components/context/AuthContext.tsx";
