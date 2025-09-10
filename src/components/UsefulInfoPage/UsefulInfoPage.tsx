2649ab9b6e5521f93352607a3edbd2ec8554a117
Stanislav Nemchenko <stas.buka@gmail.com>
2025-09-10 19:31:56 +0300

    add  /img/background to UsefulInfoPage

--- src/components/UsefulInfoPage/UsefulInfoPage.tsx
+++ src/components/UsefulInfoPage/UsefulInfoPage.tsx
@@ -2,37 +2,47 @@ import css from "./UsefulInfoPage.module.css";
 
 const UsefulInfoPage = () => {
     return (
-        <div className={css.infoContainer}>
-            <h2 className={css.title}>Useful Information</h2>
-            <div className={css.infoSection}>
-                <h3 className={css.sectionTitle}>Books</h3>
-                <ul>
-                    <li>
-                        <a href="#">Book 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Book 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Book 3</a>
-                    </li>
-                </ul>
+        <section className={css.bgUseful}>
+            <div className={css.infoContainer}>
+                <div className={css.literatureSection}>
+                    <h2 className={css.title}>Useful literature</h2>
+                    <div className={css.divider}>
+                        <svg width="150px" height="2">
+                            <use xlinkHref="#divider-line" />
+                        </svg>
+                    </div>
+                    <div className={css.infoSection}>
+                        <ul className={css.list}>
+                            <li>Testing dot.com Savin.</li>
+                            <li>A mental hospital in the hands of patients.</li>
+                            <li>Scrum. J. Sutherland.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={css.infoSection}>
+                    <h2 className={css.title}>Useful resources</h2>
+                    <div className={css.divider}>
+                        <svg width="150px" height="2">
+                            <use xlinkHref="#divider-line" />
+                        </svg>
+                    </div>
+                    <ul className={css.list}>
+                        <li>
+                            <a href="https://dou.ua">dou.ua</a>
+                        </li>
+                        <li>
+                            <a href="https://habr.com">Habr</a>
+                        </li>
+                        <li>
+                            <a href="https://facebook.com/">facebook.com/QA</a>
+                        </li>
+                        <li>
+                            <a href="https://goit.ua">goit.ua</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className={css.infoSection}>
-                <h3 className={css.sectionTitle}>Resources</h3>
-                <ul>
-                    <li>
-                        <a href="#">Resource 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Resource 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Resource 3</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        </section>
     );
 };
 
