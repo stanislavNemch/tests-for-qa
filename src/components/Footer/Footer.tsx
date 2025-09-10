886be45bee7008b8053bca652e7172493d0c1858
Stanislav Nemchenko <stas.buka@gmail.com>
2025-09-10 21:26:18 +0300

    footer done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,14 @@
 import css from "./Footer.module.css";
 import { IoMdHeart } from "react-icons/io";
+import { MdCopyright } from "react-icons/md";
 
 const Footer = () => {
     return (
         <footer className={css.footer}>
-            <p>
-                © 2021 | All Rights Reserved | Developed with{" "}
-                <IoMdHeart size={16} className={css.heartIcon} /> by GoIT
-                Students
+            <p className={css.footerText}>
+                <MdCopyright size={16} /> 2021 | All Rights Reserved | Developed
+                with <IoMdHeart size={16} className={css.heartIcon} /> by{" "}
+                <span className={css.textGoIT}>GoIT Students</span>
             </p>
         </footer>
     );
