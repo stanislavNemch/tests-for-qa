bb1b3ec63243387dd5f136992336cdcdae2a856e
Stanislav Nemchenko <stas.buka@gmail.com>
2025-09-12 23:17:33 +0300

    Footer done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,17 +7,20 @@ const Footer = () => {
         <footer>
             <section className={css.footer}>
                 <div className={css.container}>
-                    <p className={css.footerText}>
+                    <div className={css.footerText}>
                         <div className={css.copyrightLine}>
-                            <MdCopyright size={16} /> 2021 | All Rights Reserved
-                            | Developed with
+                            <MdCopyright
+                                size={18}
+                                className={css.copyrightIcon}
+                            />{" "}
+                            2021 | All Rights Reserved | Developed with
                             <IoMdHeart size={16} className={css.heartIcon} />
                         </div>
                         <div className={css.byGoIT}>
                             by{" "}
                             <span className={css.textGoIT}>GoIT Students</span>
                         </div>
-                    </p>
+                    </div>
                 </div>
             </section>
         </footer>
